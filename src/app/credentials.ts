0b0a743f6c9602f7607ee6d790e356f72ea72819
Ardian Atminanto <ardian.atminanto@gmail.com>
2019-02-27 11:11:32 +0700

    initial

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDSqEssCXyUAr39ruMV2xWfxCikBq-ySfg",
-    authDomain: "yusufrizalh-mws.firebaseapp.com",
-    databaseURL: "https://yusufrizalh-mws.firebaseio.com",
-    projectId: "yusufrizalh-mws",
-    storageBucket: "yusufrizalh-mws.appspot.com",
-    messagingSenderId: "958673904270"
-};
+    apiKey: "AIzaSyBk9ehzWS6VEggXfwCjk16TEIlW8NA1K0o",
+    authDomain: "ionicfirebase-31712.firebaseapp.com",
+    databaseURL: "https://ionicfirebase-31712.firebaseio.com",
+    projectId: "ionicfirebase-31712",
+    storageBucket: "ionicfirebase-31712.appspot.com",
+    messagingSenderId: "484246888996"
+  };
